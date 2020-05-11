9207a84c422212f585c7d1441ab15f253d0ee124
wxxeibo <wxxeibo@gmail.com>
2020-05-11 23:12:00 +0800

    move taobao

--- detail.tmall.com/init.js
+++ detail.tmall.com/init.js
@@ -1,4 +1,5 @@
-/* global $, createLogger, downloadImage2, wrapImagesWithDownloadBtn */
+/* global $, createLogger, downloadImage2, wrapImagesWithDownloadBtn, originalImage */
+/* global replaceImgSrc, wrapDownloadButtonToImage */
 
 // This script will be runned by chrome.tabs.executeScript in eventPage.js
 
@@ -9,7 +10,9 @@
     log = createLogger("detail.tmall.com/init.js");
   } catch (error) {
     console.error("failed to create logger, error:", error);
+    alert("failed to create logger");
     debugger;
+    return;
   }
 
   if (disabled) {
@@ -26,6 +29,87 @@
       return;
     }
     observer.observe(document.body, { childList: true, subtree: true });
+  };
+
+  const pressLKey = () => {
+    const imgUrls = [];
+
+    $(".tm-m-photos-thumb img").each((key, img) => {
+      log("Process image:", img);
+      const $img = $(img);
+
+      if (!$img.attr("data-xx-original-src")) {
+        // "//img.alicdn.com/bao/uploaded/i1/O1CN01NjCKv91purdU1nTSd_!!0-rate.jpg_40x40.jpg"
+        // "//img.alicdn.com/bao/uploaded/i1/O1CN01NjCKv91purdU1nTSd_!!0-rate.jpg"
+        originalImage("_40x40.jpg", "")($img);
+        replaceImgSrc("_40x40.jpg", "_400x400.jpg")($img);
+      }
+
+      imgUrls.push({
+        original: $img.attr("data-xx-original-src"),
+        thumb400: $img.attr("src")
+      });
+    });
+
+    // $(".tm-col-master").css("width", "800px"); // 一行展示更多图片
+    // $(".tm-rate-reply").remove(); // 删除店主评论文字部分（太占版面了），只看照片。
+    //
+
+    // /**
+    //  * 删除无用的块，调整块的边距
+    //  */
+    // // 删除列，比如“颜色分类”
+    // $("td.col-meta").remove();
+    // // 删除列，比如“买家 ID”
+    // $("td.col-author").remove();
+    // // 删除块，比如“初次评价”，或者“收货后追加”
+    // $("div.tm-rate-tag").remove();
+    // // 调整“初次评价”块和“收货后追加”块的边距
+    // $("div.tm-rate-premiere").css("padding", "0");
+    // $("div.tm-rate-append").css("padding", "0");
+    // // 删除顾客评论正文
+    // $("div.tm-rate-fulltxt").remove();
+
+    /**
+     * 将原来的图片列表隐藏，生成新的图片列表
+     */
+
+    $("div.rate-grid > table").hide();
+
+    // Re-create root element for photo list
+    $(".xx-photo-list").remove();
+    $("div.rate-grid").append(
+      $("<div/>", {
+        // text: "Photo List",
+        class: "xx-photo-list",
+        css: { width: "800px" }
+      })
+    );
+
+    imgUrls.forEach((item, i) => {
+      $(".xx-photo-list").append(
+        $("<div/>", {
+          class: "xx-photo-item",
+          css: {
+            float: "left",
+            position: "relative",
+            margin: "5px"
+          }
+        }).append(
+          $("<img/>", {
+            src: item.thumb400,
+            css: { width: "150px" },
+            attr: {
+              "data-xx-original-src": item.original
+            }
+          })
+        )
+      );
+    });
+
+    $(".xx-photo-list img").each((key, img) => {
+      wrapDownloadButtonToImage(img, icon, downloadImage2);
+    });
   };
 
   /**
@@ -93,6 +177,10 @@
     log("eventHandler", event, event.code);
 
     switch (event.code) {
+      case "KeyL":
+        pressLKey();
+        break;
+
       case "KeyR":
         // Put the original image src in the data attribute, so the downloader could download the original images.
         $("#description img").each((index, img) => {
@@ -120,6 +208,9 @@
       case "ArrowRight": {
         log("ArrowRight pressed");
         turnToNextPage();
+        setTimeout(() => {
+          pressLKey();
+        }, 1000);
 
         break;
       }
